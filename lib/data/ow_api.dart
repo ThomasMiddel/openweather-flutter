348f3a63e28800c0f1a91ceec7502595c97ad4a6
Thomas <thomas.middel@gmail.com>
2020-05-11 09:47:51 +0200

    + Clean up API

--- lib/data/ow_api.dart
+++ lib/data/ow_api.dart
@@ -5,15 +5,11 @@ import 'weather/weather_response.dart';
 
 part 'ow_api.g.dart';
 
-@RestApi(baseUrl: "https://community-open-weather-map.p.rapidapi.com")
+@RestApi()
 abstract class OwApi {
   factory OwApi(Dio dio, {String baseUrl}) = _OwApi;
 
-  @GET("/weather")
-  @Headers(<String, String>{
-    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-    "x-rapidapi-key": "9100e383f4msh72f066e78203fbdp181e60jsn74e9a0ec70b8"
-  })
+  @GET('/weather')
   Future<WeatherResponse> getCurrentWeather(
     @Query('q') String location,
   );
